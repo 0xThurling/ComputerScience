f7159f59233c19e32414ede0cd3870bad598e2f0
Jacques Thurling <j.thurling@capital-sa.com>
2023-01-10 11:50:39 +0200

    [Update]: Started Week 3

--- IntroductionToComputerScience/WeekThree/search.c
+++ IntroductionToComputerScience/WeekThree/search.c
@@ -6,5 +6,15 @@ int main(void){
 
     int n = get_int("Number: ");
 
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
     
+    printf("Not found\n");
+    return 1;
 }
