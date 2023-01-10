f7e0565b4776c8b3591c71e4b7ee38920dca1025
Jacques Thurling <j.thurling@capital-sa.com>
2023-01-10 22:22:24 +0200

    [Updated]: Busy with pset week 3

---
+++ IntroductionToComputerScience/WeekThree/pset/runoff.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+typedef struct
+{
+    string rank1;
+    string rank2;
+    string rank3;
+} vote;
+
+typedef struct
+{
+    int alice;
+    int bob;
+    int charlie;
+} results;
+
+results apply_votes(string rank, int point);
+
+int main(void)
+{
+    int amount = get_int("Number of voters: ");
+    
+    vote votes[amount];
+    results result;
+
+    for (int i = 0; i < amount; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            string rank = get_string("Rank %i: ", j + 1);
+            result = apply_votes(rank, j + 1);
+        }
+        printf("\n");
+    }
+
+    if (result.alice <= result.bob && result.bob < result.charlie)
+    {
+        printf("%s\n", "Charlie");
+        return 0;
+    }
+
+    if (result.alice <= result.charlie && result.charlie < result.bob)
+    {
+        printf("%s\n", "Bob");
+        return 0;
+    }
+
+    if (result.charlie <= result.bob && result.bob < result.alice)
+    {
+        printf("%s\n", "Charlie");
+        return 0;
+    }
+    
+    
+}
+
+results apply_votes(string rank, int point)
+{
+    results result;
+    if (!strcmp(rank, "Alice") && point == 1)
+    {
+        result.alice += 3;
+    } else if (!strcmp(rank, "Alice") && point == 2)
+    {
+        result.alice += 2;
+    } else if (!strcmp(rank, "Alice")  && point == 3) {
+        result.alice += 1;
+    }
+
+    if (!strcmp(rank, "Bob") && point == 1)
+    {
+        result.bob += 3;
+    } else if (!strcmp(rank, "Bob") && point == 2)
+    {
+        result.bob += 2;
+    } else if (!strcmp(rank, "Bob") && point == 3) {
+        result.bob += 1;
+    }
+
+    if (!strcmp(rank, "Charlie") && point == 1)
+    {
+        result.charlie += 3;
+    } else if (!strcmp(rank, "Charlie") && point == 2)
+    {
+        result.charlie += 2;
+    } else if (!strcmp(rank, "Charlie") && point == 3) {
+        result.charlie += 1;
+    }
+}
