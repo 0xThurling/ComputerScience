4d5e43906d5fd1d4e4a2d0429830d7dbf92ea4f9
Jacques Thurling <j.thurling@capital-sa.com>
2023-01-23 14:12:10 +0200

    [Updated]: Made Updates

--- IntroductionToComputerScience/WeekFive/pset/speller/dictionary.c
+++ IntroductionToComputerScience/WeekFive/pset/speller/dictionary.c
@@ -26,14 +26,46 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
-    return false;
+    if (strcmp(word, " "))
+    {
+        int hash_index = hash(word);
+        if (hash_index >= 0)
+        {
+            node *ptr = table[hash_index];
+            if (ptr == NULL)
+            {
+                return true;
+            } else
+            {
+                int compare = 0;
+                while (ptr != NULL)
+                {
+                    compare = strcmp(ptr->word, word);
+                    ptr = ptr->next;
+                }
+                return compare == 0;
+            }
+        }
+    }
+    
+    //printf("%i\n", word[0]);
+    
+    
+    // while (ptr->next != NULL)
+    // {
+    //     printf("%s\n", ptr->word);
+    //     ptr = ptr->next;
+    // }
+    
+
+    return true;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    return (toupper(word[0]) - 'A') + (toupper(word[1]) - 'A');
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -50,22 +82,62 @@ bool load(const char *dictionary)
     int size = ftell(chosen_dictionary); // get current file pointer
     fseek(chosen_dictionary, 0, SEEK_SET);
 
-    char c;
-    for (int i = 0; i < size; i++)
+    //Holds word temporarily in memory
+    char buffer[LENGTH + 1];
+    
+    for (int i = 0, buffer_i = 0; i < size; i++)
     {
-        fread(&c, sizeof(char), sizeof(char), chosen_dictionary);
-        if (c == 10)
-        {
-            printf("%c %i\n", c, c);
-        }
-    }
+        //Holds word temporaroly
+        fread(&buffer[buffer_i], sizeof(char), sizeof(char), chosen_dictionary);
+        
 
-    printf("\n");
+        if (buffer[buffer_i] == 10)
+        {
+            int hash_table_index = hash(buffer);
+            if (table[hash_table_index] == 0)
+            {
+                // Adds a new node to the hash table is there isn't a value
+                // Allocates memory for a word
+                node *new_node = malloc(sizeof(node));
+                buffer[ strcspn(buffer, "\r\n") ] = '\0';
+                strcpy(new_node->word, buffer);
+                new_node->next = NULL;
+
+                // Adds node to the hash table
+                table[hash_table_index] = new_node;
+            } else
+            {
+                // Gets first item
+                node *head = table[hash_table_index];
+
+                // Adds item to the front
+                node *new_node = malloc(sizeof(node));
+                buffer[ strcspn(buffer, "\r\n") ] = '\0';
+                strcpy(new_node->word, buffer);
+                new_node->next = head;
+
+
+                if (new_node->next == NULL)
+                {
+                    return false;
+                }
+
+                table[hash_table_index] = new_node;
+            }
+
+            buffer_i = 0;
+
+            // Continue to next word
+            continue;
+        }
+
+        buffer_i++;
+    }
     
     
     fclose(chosen_dictionary);
 
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
